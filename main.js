5011df1d12e0e73e0a0231f3dd014c47930bd6b3
Andrew Red <andrew@reddikh.com>
2020-05-22 12:21:29 +0300

    re-wrote the original code to make it reusable, fixed issue when dns does not exist

--- main.js
+++ main.js
@@ -1,65 +1,65 @@
 /**
- * Create DNS Record Action for GitHub
- * https://github.com/marketplace/actions/create-dns-record
+ * Delete Cloudflare DNS Record Action for GitHub
+ * https://github.com/marketplace/actions/cloudflare-delete-dns-record
  */
 
 const cp = require("child_process");
 
-let res;
-let result;
-let id = process.env.INPUT_ID;
-
-if (!id) {
-  result = cp.spawnSync("curl", [
+const getCurrentRecordId = () => {
+  //https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
+  const { status, stdout } = cp.spawnSync("curl", [
     ...["--header", `Authorization: Bearer ${process.env.INPUT_TOKEN}`],
     ...["--header", "Content-Type: application/json"],
     `https://api.cloudflare.com/client/v4/zones/${process.env.INPUT_ZONE}/dns_records`,
   ]);
 
-  if (result.status !== 0) {
-    process.exit(result.status);
+  if (status !== 0) {
+    process.exit(status);
   }
 
-  res = JSON.parse(result.stdout.toString());
+  const { success, result, errors } = JSON.parse(stdout.toString());
 
-  if (!res.success) {
-    console.log(`::error ::${res.errors[0].message}`);
+  if (!success) {
+    console.log(`::error ::${errors[0].message}`);
     process.exit(1);
   }
 
   const name = process.env.INPUT_NAME;
-  const record = res.result.find((x) => x.name === name);
+  const record = result.find((x) => x.name === name);
 
   if (!record) {
-    console.log('::error ::Cannot find "${name}" record.');
-    process.exit(1);
+    return null
   }
 
-  id = record.id;
+  return record.id;
 }
 
+const deleteRecord = (id) => {
+  // https://api.cloudflare.com/#dns-records-for-a-zone-delete-dns-record
+  const { status, stdout } = cp.spawnSync("curl", [
+    ...["--silent", "--request", "DELETE"],
+    ...["--header", `Authorization: Bearer ${process.env.INPUT_TOKEN}`],
+    ...["--header", "Content-Type: application/json"],
+    `https://api.cloudflare.com/client/v4/zones/${process.env.INPUT_ZONE}/dns_records/${id}`,
+  ]);
+
+  if (status !== 0) {
+    process.exit(status);
+  }
+
+  const { success, result, errors } = JSON.parse(stdout.toString());
+
+  if (!success) {
+    console.log(`::error ::${errors[0].message}`);
+    process.exit(1);
+  }
+}
+
+const id = process.env.INPUT_ID || getCurrentRecordId();
 if (!id) {
-  console.log(`::error ::DNS record ID or record name is required.`);
-  process.exit(1);
+  console.log("Record doesn't exist. Nothing to delete.");
+  process.exit(0);
 }
+deleteRecord(id);
 
-// https://api.cloudflare.com/#dns-records-for-a-zone-delete-dns-record
-result = cp.spawnSync("curl", [
-  ...["--silent", "--request", "DELETE"],
-  ...["--header", `Authorization: Bearer ${process.env.INPUT_TOKEN}`],
-  ...["--header", "Content-Type: application/json"],
-  `https://api.cloudflare.com/client/v4/zones/${process.env.INPUT_ZONE}/dns_records/${id}`,
-]);
 
-if (result.status !== 0) {
-  process.exit(result.status);
-}
-
-res = JSON.parse(result.stdout.toString());
-
-if (!res.success) {
-  console.log(`::error ::${res.errors[0].message}`);
-  process.exit(1);
-}
-
-console.dir(res.result);
